fbb07dd20bd50614c12d30f742d961db0dab1a81
Anuwat Khongchuai <gh0stsync.rt@gmail.com>
2023-01-29 19:14:56 +0700

    Add files via upload

---
+++ hello.ino
@@ -0,0 +1,13 @@
+// C++ code
+//
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("Hello Arduino Programming");
+  Serial.println("I love Arduino Programming");
+  delay(1000);
+}
